e79953f963f4d3e2c54f53513ca05f3750f01f86
Dwayne Slater <ds84182@gmail.com>
2018-03-21 16:59:58 -0400

    Migrate from Endianness.SCREAMING_CASE to Endian.lowercase

--- test/src/ufixnum_test.dart
+++ test/src/ufixnum_test.dart
@@ -77,34 +77,34 @@ void main() {
       expect(clip16(0x10000), 0x0000);
     });
 
-    test( "pack16(BIG_ENDIAN)", () {
+    test( "pack16(big endian)", () {
       var out = new Uint8List(2);
-      pack16(0x1020, out, 0, Endianness.BIG_ENDIAN);
+      pack16(0x1020, out, 0, Endian.big);
       expect(out[0], 0x10);
       expect(out[1], 0x20);
     });
 
-    test( "pack16(LITTLE_ENDIAN)", () {
+    test( "pack16(little endian)", () {
       var out = new Uint8List(2);
-      pack16(0x1020, out, 0, Endianness.LITTLE_ENDIAN);
+      pack16(0x1020, out, 0, Endian.little);
       expect(out[1], 0x10);
       expect(out[0], 0x20);
     });
 
-    test( "unpack16(BIG_ENDIAN)", () {
+    test( "unpack16(big endian)", () {
       var inp = new Uint8List.fromList([0x10, 0x20]);
-      expect(unpack16(inp, 0, Endianness.BIG_ENDIAN), 0x1020);
+      expect(unpack16(inp, 0, Endian.big), 0x1020);
     });
 
-    test( "unpack16(LITTLE_ENDIAN)", () {
+    test( "unpack16(little endian)", () {
       var inp = new Uint8List.fromList([0x20, 0x10]);
-      expect(unpack16(inp, 0, Endianness.LITTLE_ENDIAN), 0x1020);
+      expect(unpack16(inp, 0, Endian.little), 0x1020);
     });
 
     test( "pack16(Uint8List.view)", () {
       var out = new Uint8List(6);
       out = new Uint8List.view(out.buffer, 2, 2);
-      pack16(0x1020, out, 0, Endianness.BIG_ENDIAN);
+      pack16(0x1020, out, 0, Endian.big);
       expect(out[0], 0x10);
       expect(out[1], 0x20);
     });
@@ -112,7 +112,7 @@ void main() {
     test( "unpack16(Uint8List.view)", () {
       var inp = new Uint8List.fromList([0, 0, 0x20, 0x10, 0, 0]);
       inp = new Uint8List.view(inp.buffer, 2, 2);
-      expect(unpack16(inp, 0, Endianness.LITTLE_ENDIAN), 0x1020);
+      expect(unpack16(inp, 0, Endian.little), 0x1020);
     });
 
   });
@@ -174,38 +174,38 @@ void main() {
       expect(rotr32(0x10203040, 32), 0x10203040);
     });
 
-    test( "pack32(BIG_ENDIAN)", () {
+    test( "pack32(big endian)", () {
       var out = new Uint8List(4);
-      pack32(0x10203040, out, 0, Endianness.BIG_ENDIAN);
+      pack32(0x10203040, out, 0, Endian.big);
       expect(out[0], 0x10);
       expect(out[1], 0x20);
       expect(out[2], 0x30);
       expect(out[3], 0x40);
     });
 
-    test( "pack32(LITTLE_ENDIAN)", () {
+    test( "pack32(little endian)", () {
       var out = new Uint8List(4);
-      pack32(0x10203040, out, 0, Endianness.LITTLE_ENDIAN);
+      pack32(0x10203040, out, 0, Endian.little);
       expect(out[3], 0x10);
       expect(out[2], 0x20);
       expect(out[1], 0x30);
       expect(out[0], 0x40);
     });
 
-    test( "unpack32(BIG_ENDIAN)", () {
+    test( "unpack32(big endian)", () {
       var inp = new Uint8List.fromList([0x10, 0x20, 0x30, 0x40]);
-      expect(unpack32(inp, 0, Endianness.BIG_ENDIAN), 0x10203040);
+      expect(unpack32(inp, 0, Endian.big), 0x10203040);
     });
 
-    test( "unpack32(LITTLE_ENDIAN)", () {
+    test( "unpack32(little endian)", () {
       var inp = new Uint8List.fromList([0x40, 0x30, 0x20, 0x10]);
-      expect(unpack32(inp, 0, Endianness.LITTLE_ENDIAN), 0x10203040);
+      expect(unpack32(inp, 0, Endian.little), 0x10203040);
     });
 
     test( "pack32(Uint8List.view)", () {
       var out = new Uint8List(8);
       out = new Uint8List.view(out.buffer, 2, 4);
-      pack32(0x10203040, out, 0, Endianness.BIG_ENDIAN);
+      pack32(0x10203040, out, 0, Endian.big);
       expect(out[0], 0x10);
       expect(out[1], 0x20);
       expect(out[2], 0x30);
@@ -215,7 +215,7 @@ void main() {
     test( "unpack32(Uint8List.view)", () {
       var inp = new Uint8List.fromList([0, 0, 0x40, 0x30, 0x20, 0x10, 0, 0]);
       inp = new Uint8List.view(inp.buffer, 2, 4);
-      expect(unpack32(inp, 0, Endianness.LITTLE_ENDIAN), 0x10203040);
+      expect(unpack32(inp, 0, Endian.little), 0x10203040);
     });
 
   });
@@ -576,9 +576,9 @@ void main() {
           new Register64(0x10203040, 0x05050505)..rotr(64), new Register64(0x10203040, 0x05050505));
     });
 
-    test( "pack(BIG_ENDIAN)", () {
+    test( "pack(big endian)", () {
       var out = new Uint8List(64);
-      new Register64(0x10203040, 0x50607080).pack(out, 0, Endianness.BIG_ENDIAN);
+      new Register64(0x10203040, 0x50607080).pack(out, 0, Endian.big);
       expect(out[0], 0x10);
       expect(out[1], 0x20);
       expect(out[2], 0x30);
@@ -589,9 +589,9 @@ void main() {
       expect(out[7], 0x80);
     });
 
-    test( "pack(LITTLE_ENDIAN)", () {
+    test( "pack(little endian)", () {
       var out = new Uint8List(64);
-      new Register64(0x10203040, 0x50607080).pack(out, 0, Endianness.LITTLE_ENDIAN);
+      new Register64(0x10203040, 0x50607080).pack(out, 0, Endian.little);
       expect(out[7], 0x10);
       expect(out[6], 0x20);
       expect(out[5], 0x30);
@@ -602,21 +602,21 @@ void main() {
       expect(out[0], 0x80);
     });
 
-    test( "unpack(BIG_ENDIAN)", () {
+    test( "unpack(big endian)", () {
       var inp = new Uint8List.fromList([0x10, 0x20, 0x30, 0x40, 0x50, 0x60, 0x70, 0x80]);
-      expect(new Register64()..unpack(inp, 0, Endianness.BIG_ENDIAN), new Register64(0x10203040, 0x50607080));
+      expect(new Register64()..unpack(inp, 0, Endian.big), new Register64(0x10203040, 0x50607080));
     });
 
-    test( "unpack(LITTLE_ENDIAN)", () {
+    test( "unpack(little endian)", () {
       var inp = new Uint8List.fromList([0x80, 0x70, 0x60, 0x50, 0x40, 0x30, 0x20, 0x10]);
       expect(
-         new Register64()..unpack(inp, 0, Endianness.LITTLE_ENDIAN), new Register64(0x10203040, 0x50607080));
+         new Register64()..unpack(inp, 0, Endian.little), new Register64(0x10203040, 0x50607080));
     });
 
     test( "pack(Uint8List.view)", () {
       var out = new Uint8List(68);
       out = new Uint8List.view(out.buffer, 2, 64);
-      new Register64(0x10203040, 0x50607080).pack(out, 0, Endianness.BIG_ENDIAN);
+      new Register64(0x10203040, 0x50607080).pack(out, 0, Endian.big);
       expect(out[0], 0x10);
       expect(out[1], 0x20);
       expect(out[2], 0x30);
@@ -627,11 +627,11 @@ void main() {
       expect(out[7], 0x80);
     });
 
-    test( "unpack(LITTLE_ENDIAN)", () {
+    test( "unpack(little endian)", () {
       var inp = new Uint8List.fromList([0, 0, 0x80, 0x70, 0x60, 0x50, 0x40, 0x30, 0x20, 0x10, 0, 0]);
       inp = new Uint8List.view(inp.buffer, 2, 8);
       expect(
-          new Register64()..unpack(inp, 0, Endianness.LITTLE_ENDIAN), new Register64(0x10203040, 0x50607080));
+          new Register64()..unpack(inp, 0, Endian.little), new Register64(0x10203040, 0x50607080));
     });
 
     test( "toString()", () {
